97caa6d1a076e361a382701a3fa1d7b3979dfe4b
INHYUK-RYU <inhyuk-ryu@INHYUK-RYUui-MacBookPro.local>
2023-10-19 22:59:21 +0900

    부탁입니다

---
+++ src/app/list/page.js
@@ -0,0 +1,90 @@
+import Image from 'next/image'
+import 파이썬 from '/Users/inhyuk-ryu/cloud/public/파이썬아이콘-removebg-preview.png'
+import 유니티 from '/Users/inhyuk-ryu/cloud/public/유니티아이콘2-removebg-preview.png'
+import 아두이노 from '/Users/inhyuk-ryu/cloud/public/아두이노아이콘-removebg-preview.png'
+import 스크래치 from '/Users/inhyuk-ryu/cloud/public/스크래치아이콘-removebg-preview.png'
+import Link from "next/link"
+
+
+export default function Home() {
+    return(
+     
+   <div>
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto flex flex-wrap">
+    <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm"></div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <Image src={파이썬}/>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">파이썬</h2>
+          <p className="leading-relaxed">AI 분석, py게임</p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm"></div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <Image src={유니티}/>
+
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">유니티</h2>
+          <p className="leading-relaxed">3D 죽림고수, 박민창굴리기, 알까기, 베리나 퀴즈</p>
+        </div>
+      </div>
+    </div>
+    <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm"></div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+         <Image src={아두이노}/>
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">아두이노</h2>
+          <p className="leading-relaxed">스마트팜, 공기청정기, </p>
+        </div> 
+        <div className='go_button'>
+          
+        <Link href='https://drive.google.com/drive/folders/11oQPZZHa4q4EPJJdXhiXrwr9Wy0jt5fl?usp=share_link' > 
+          아두이노 보러가기
+          </Link>
+          </div>
+
+      </div>
+    </div>
+    <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+      <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm"></div>
+      <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div className="flex-shrink-0 w-24 h-24 bg-indigo-100 text-indigo-500 rounded-full inline-flex items-center justify-center">
+        <Image src={스크래치}/>
+
+        </div>
+        <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">스크래치</h2>
+          <p className="leading-relaxed">선인 Run Away, 벽돌깨기, 착시현상 </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+   </div>
+    )
+  
+  }
